0526fe5652d093baf66384de8ba0d9e44edff9f1
maadZia <115706373+maadZia@users.noreply.github.com>
2024-05-17 12:09:49 +0200

    Create ex1.java

---
+++ example/ex1.java
@@ -0,0 +1,9 @@
+//kod z pierwszego parsetree
+
+public static class Main {
+        public void main(String[] args) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("wartosc i: " + i);
+            }
+        }
+}
