cf17a76ec23957657250f0e11b1352e96291330c
maadZia <115706373+maadZia@users.noreply.github.com>
2024-06-06 12:13:23 +0200

    Update and rename example/ex2.java to example/code/ex2.java

--- example/code/ex2.java
+++ example/code/ex2.java
@@ -1,21 +1,18 @@
 //second parsetree code
 
 public class ExampleClass {
-    // Zmienne globalne
     private int variable1 = 10;
     private String variable2 = "Hello";
 
-    // Funkcja 1
     public void printGlobalVariables() {
+        int a = 5;
         System.out.println("wartosc zmiennej 1: " + variable1);
         System.out.println("wartosc zmiennej 2: " + variable2);
+        System.out.println("wartosc zmiennej a: " + a);
     }
 
-    // metoda main
     public static void main(String[] args) {
         ExampleClass example = new ExampleClass();
-
-        // wywołanie funkcji 
         example.printGlobalVariables();
     }
 }
